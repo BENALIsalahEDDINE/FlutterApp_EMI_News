f2990c5fccb307eaa4eb4e1ee7977e61979176c5
Salah Eddine <salaheddinebenali@student.emi.ac.ma>
2020-12-04 22:51:29 +0100

    article_model by salah eddine

---
+++ lib/helper/news.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter_emi_news/models/article_model.dart';
+import 'package:http/http.dart' as http;
+class News{
+
+  List<ArticleModel> news = [];
+
+  Future<void> getNews() async{
+
+    String url = "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86224fe7511e4121a9cb03579d9e7aa4";
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+    if(jsonData['status'] == 'ok'){
+      jsonData["articles"].forEach((element){
+
+        if(element["urlToImage"] != null && element["description"] != null){
+          ArticleModel articleModel = ArticleModel()
+        }
+      });
+    }
+  }
+
+}
---
+++ lib/models/article_model.dart
@@ -0,0 +1,10 @@
+class ArticleModel{
+  String author ;
+  String title ;
+  String description;
+  String url;
+  String urlImage;
+  String content;
+  ArticleModel({this.author,this.title,this.url,this.description,this.urlImage,this.content});
+
+}
