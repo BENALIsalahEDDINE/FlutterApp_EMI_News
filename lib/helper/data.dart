62c313ae1e16058fcfefdfdb09cd6470016c445a
BENALIsalahEDDINE <salaheddinebenali@student.emi.ac.ma>
2020-12-09 02:18:49 +0100

    data by salah

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -6,6 +6,9 @@ List<CategoryModel> getCategories(){
   CategoryModel categoryModel = new CategoryModel();
 
 
+
+
+
   //1
   categoryModel.categoryName = "Business";
   categoryModel.imageUrl = "https://www.emi.ac.ma/images/stories/baniere12112012/SNC102700.jpg";
